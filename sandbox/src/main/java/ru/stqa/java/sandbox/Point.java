3876a1f25e7447cd7e760f0dec10308dd4bf1479
eSeemann <eSeemann@users.noreply.github.com>
2017-09-19 11:51:57 +0200

    ДЗ

--- sandbox/src/main/java/ru/stqa/java/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/Point.java
@@ -12,4 +12,19 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    }
+
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(5, 10);
+        Point p2 = new Point(10, 10);
+
+        System.out.println("Расстояние между точкой p1 и p2 = " + distance(p1, p2));
+    }
+
 }
+
