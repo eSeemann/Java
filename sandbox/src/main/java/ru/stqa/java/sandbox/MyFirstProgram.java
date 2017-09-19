64f663e7ca7a339cce9dcb96a20b5e6100f88c25
eSeemann <eSeemann@users.noreply.github.com>
2017-09-19 12:37:12 +0200

    1

--- sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProgram.java
@@ -14,14 +14,6 @@ public class MyFirstProgram {
 
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p1 = new Point(5, 10);
-        Point p2 = new Point(10, 10);
-
-        System.out.println("Расстояние между точками p1 и p2 = " +distance(p1,p2));
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
     }
 
     public static void hello(String somebody) {
