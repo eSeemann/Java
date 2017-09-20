65cc776ecfc814624d8770c088f3a761f199acb1
eSeemann <eSeemann@users.noreply.github.com>
2017-09-20 16:47:02 +0200

    Переделано  ДЗ_PointTests

--- sandbox/src/test/java/ru/stqa/java/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/java/sandbox/PointTests.java
@@ -12,20 +12,20 @@ public class PointTests {
     public void testDistance() {
         Point p1 = new Point(5, 10);
         Point p2 = new Point(10, 10);
-        assert p1.distance(p1, p2) == 5.0;
+        assert p1.distance(p2) == 5.0;
     }
 
     @Test
     public void testDistance2() {
         Point p1 = new Point(100, 100);
         Point p2 = new Point(100, 100);
-        assert p1.distance(p1, p2) == 0;
+        assert p1.distance(p2) == 0;
     }
 
     @Test
     public void testDistance3() {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(50, 50);
-        Assert.assertEquals(p1.distance(p1, p2), 50);
+        Assert.assertEquals(p1.distance(p2), 50);
     }
 }
