067469e47ff93729362b132fb633643ba92f786e
eSeemann <eSeemann@users.noreply.github.com>
2017-09-25 19:39:25 +0200

    Создан тест для проверки удаления контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/tests/ContactDeletionTests.java
@@ -0,0 +1,50 @@
+package ru.stqa.java.addressbook.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactDeletionTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void ContactDeletionTests() {
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        wd.findElement(By.name("selected[]")).click();
+        wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+        wd.switchTo().alert().accept();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
