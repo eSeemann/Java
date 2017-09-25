04521b41d6835c110985b05834deca2e82b0f5cf
eSeemann <eSeemann@users.noreply.github.com>
2017-09-25 19:41:30 +0200

    rename method

--- addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/tests/ContactDeletionTests.java
@@ -20,7 +20,7 @@ public class ContactDeletionTests {
     }
     
     @Test
-    public void ContactDeletionTests() {
+    public void testContactDeletion() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
