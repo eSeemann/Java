a8ddb722559dcace2a4c23a6174544d22aaa996f
eSeemann <eSeemann@users.noreply.github.com>
2017-09-25 21:43:03 +0200

    Создан базовый класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/appmanager/ContactHelper.java
@@ -7,34 +7,14 @@ import ru.stqa.java.addressbook.model.ContactData;
 /**
  * Created by eSeemann on 25.09.2017.
  */
-public class ContactHelper {
-    private FirefoxDriver wd;
+public class ContactHelper extends BaseHelper {
 
     public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void returnToHomePage() {
         wd.findElement(By.linkText("home page")).click();
-    }
-
-    public void submitContactCreation() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getVorname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getNachname());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getHandy());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getMailto());
     }
 
     public void initNewContact() {
--- addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java/addressbook/appmanager/GroupHelper.java
@@ -7,31 +7,14 @@ import ru.stqa.java.addressbook.model.GroupData;
 /**
  * Created by eSeemann on 25.09.2017.
  */
-public class GroupHelper {
-    private FirefoxDriver wd;
+public class GroupHelper extends BaseHelper {
 
     public GroupHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void returnToGroupPage() {
         wd.findElement(By.linkText("group page")).click();
-    }
-
-    public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    public void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
     public void initNewGroup() {
